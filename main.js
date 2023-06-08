4b36f1dcbba35ce16dcaaa99294205db48fb37d6
Chavot <ukuharm@gmail.com>
2023-06-09 01:10:11 +0300

    project upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// opens hamburger menu
+function openMenu() {
+  document.getElementById("hamburgerMenu").style.width = "100%";
+}
+// closes hamburger menu
+function closeMenu() {
+  document.getElementById("hamburgerMenu").style.width = "0%";
+}
